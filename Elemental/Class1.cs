142ae61f6625d838ebe475ded4b9e7ed90f8dd65
esterovadaniela@orangedom.orangefactory.cz <esterovadaniela@orangedom.orangefactory.cz>
2018-04-03 13:46:21 +0200

    Přidejte soubory projektu.

---
+++ Elemental/Class1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elemental
+{
+    class Button
+    {
+
+        int height;
+        int width;
+        public Vector2 position;
+        public Texture2D texture;
+        public Color color;
+
+        public Button(Vector2 position, int height, int width, Color color, GraphicsDevice graphics) {
+
+            this.position = position;
+            this.width = width;
+            this.height = height;
+            this.color = color;
+
+            texture = new Texture2D(graphics, width, height);
+            Color[] button = new Color[width * height];
+
+            for (int i = 0; i < button.Length; i++)
+            {
+                button[i] = Color.Cyan;
+
+            }
+            texture.SetData(button);
+
+
+
+        }
+
+
+    }
+}
---
+++ Elemental/Game1.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Elemental
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D texture;
+        Vector2 position;
+        Button tlacitko;
+        int width = 50;
+        int height = 50;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 400;
+            graphics.PreferredBackBufferWidth = 800;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            position = new Vector2(1, 350);
+            texture = new Texture2D(this.GraphicsDevice, width, height);
+            Color[] square = new Color[width * height];
+
+            // naplníme square barvou
+
+            tlacitko = new Button(new Vector2(10, 10), 10, 10, Color.AntiqueWhite, GraphicsDevice);
+
+            for (int i = 1; i < square.Length; i++)
+            {
+                square[i] = Color.Khaki;
+            }
+
+            texture.SetData(square);
+            base.Initialize();
+        }
+
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+           // tlacitko.texture = Content.Load<Texture2D>("texture");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState state = Keyboard.GetState();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (state.IsKeyDown(Keys.Right))
+                position.X += 2;
+            if (state.IsKeyDown(Keys.Left))
+                position.X -= 2;
+            if (state.IsKeyDown(Keys.Up))
+                position.Y -= 2;
+            if (state.IsKeyDown(Keys.Down))
+                position.Y += 0;
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+
+            GraphicsDevice.Clear(Color.DarkSlateGray);
+
+            // TODO: Add your drawing code here
+
+            
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, position);
+            spriteBatch.Draw(tlacitko.texture, tlacitko.position);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
